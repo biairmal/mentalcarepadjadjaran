d020bf0ad59c8c0c5ec4178ce6888683c9592802
Fahrul Azimi <fahrul.azimi.2000@gmail.com>
2020-11-24 22:15:36 +0700

    update login

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:mcp/pages/splashscreen_view.dart';
-
+// import 'package:mcp/pages/home_view.dart';
+// import 'package:mcp/pages/login_view.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Splash Screen',
-      theme: ThemeData(fontFamily: 'Roboto', primarySwatch: Colors.blue, buttonColor: Color(0xFFE0AD42)),
-      home: SplashScreenPage(),
-    ));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Splash Screen',
+    theme: ThemeData(
+        fontFamily: 'Roboto',
+        primarySwatch: Colors.blue,
+        buttonColor: Color(0xFFE0AD42)),
+    home: SplashScreenPage(),
+  ));
 }
 
+// class MainScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamBuilder<User>(
+//         stream: FirebaseAuth.instance.authStateChanges(),
+//         builder: (context, snapshot) {
+//           if (snapshot.hasData && snapshot.data != null) {
+//             return HomePage();
+//           }
+//           return LoginPage();
+//         });
+//   }
+// }
