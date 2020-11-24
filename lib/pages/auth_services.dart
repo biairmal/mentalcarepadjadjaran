db9f340348fbfd42a8f3d8c34c06d89cbc96b304
Fahrul Azimi <fahrul.azimi.2000@gmail.com>
2020-11-24 22:15:59 +0700

    update login

--- lib/pages/auth_services.dart
+++ lib/pages/auth_services.dart
@@ -3,28 +3,24 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthServices {
   static FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // create Account
-  static Future<SignInSignUpResult> createUser(
-      {String email, String pass}) async {
-    try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(
-          email: email, password: pass);
-      return SignInSignUpResult(user: result.user);
-    } catch (e) {
-      return SignInSignUpResult(message: e.toString());
-    }
-  }
+  // // create Account
+  // static Future<SignInSignUpResult> createUser(
+  //     {String email, String pass}) async {
+  //   try {
+  //     UserCredential result = await _auth.createUserWithEmailAndPassword(
+  //         email: email, password: pass);
+  //     return SignInSignUpResult(user: result.user);
+  //   } catch (e) {
+  //     return SignInSignUpResult(message: e.toString());
+  //   }
+  // }
 
   // sign in with email/password
-  static Future<SignInSignUpResult> signInWithEmail(
-      {String email, String pass}) async {
-    try {
-      UserCredential result =
-          await _auth.signInWithEmailAndPassword(email: email, password: pass);
-      return SignInSignUpResult(user: result.user);
-    } catch (e) {
-      return SignInSignUpResult(message: e.toString());
-    }
+  static signInWithEmail({String email, String password}) async {
+    final res = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    final User user = res.user;
+    return user;
   }
 
   static void signOut() {
